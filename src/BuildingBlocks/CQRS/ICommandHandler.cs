98647a8151cddcd5032e91aca86f0e9ce6aa4571
Alexander Mikalauskas <xjmikalauskas@gmail.com>
2025-03-01 09:50:18 -0600

    This is all that is needed before the start of section 6.

---
+++ src/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+namespace BuildingBlocks.CQRS;
+
+// If no TResponse/response is returned, return a default of Unit.
+public interface ICommandHandler<in TCommand>
+    : ICommandHandler<TCommand, Unit>
+    where TCommand : ICommand<Unit>
+{
+
+}
+
+public interface ICommandHandler<in TCommand, TResponse>
+    : IRequestHandler<TCommand, TResponse>
+    where TCommand : ICommand<TResponse>
+    where TResponse : notnull
+{
+
+}
---
+++ src/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,37 @@
+﻿namespace Catalog.API.Products.CreateProduct;
+
+// Ctrl + B --> opens and closes folders structure.
+public record CreateProductRequest(string Name, List<string> Category, string Description,
+    string ImageFile, decimal Price);
+
+public record CreateProductResponse(Guid Id);
+
+public class CreateProductEndpoint : ICarterModule
+{
+
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        // 1. Define HTTP Post endpoint uri
+        app.MapPost("/products",
+            async (CreateProductRequest request, ISender sender) => {
+                // 2. Map the request object to the command object
+                var command = request.Adapt<CreateProductCommand>();
+
+                // 3. Trigger the MediatR via sender from the MediatR class
+                var result = await sender.Send(command);
+
+                // 4. Map the result back to the response model
+                var response = result.Adapt<CreateProductResponse>();
+
+                // 5. Return the result with response and uri
+                return Results.Created($"/products/{response.Id}", response);
+            })
+        .WithName("CreateProduct")
+        .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Create Product")
+        .WithDescription("Create Product");
+        // These are all extension methods for helping to configure the HTTP POST endpoint even more.
+    }
+
+}
