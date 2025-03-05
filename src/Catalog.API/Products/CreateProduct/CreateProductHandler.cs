70315c513043426933a24e0661bc64611280a33c
Alexander Mikalauskas <xjmikalauskas@gmail.com>
2025-03-05 09:21:56 -0600

    Pushing up changes before running Docker for creating postgres db...

--- src/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,6 +1,4 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-namespace Catalog.API.Products.CreateProduct;
+﻿namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductCommand(string Name, List<string> Category, string Description,
     string ImageFile, decimal Price)
@@ -8,7 +6,7 @@ public record CreateProductCommand(string Name, List<string> Category, string De
 
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler
+internal class CreateProductCommandHandler(IDocumentSession session)
     : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
@@ -24,8 +22,10 @@ internal class CreateProductCommandHandler
             Price = command.Price
         };
         // 2. Save to Database
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
 
         // 3. Return Result
-        return new CreateProductResult(Guid.NewGuid());
+        return new CreateProductResult(product.Id);
     }
 }
